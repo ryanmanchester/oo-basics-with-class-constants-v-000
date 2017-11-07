4fd1280915f8d21db8c23b943784ddbbba20557a
Ryan Manchester <ryanm17@gmail.com>
2017-11-07 18:34:35 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,10 +7,10 @@ class Shoe
   def initialize(brand)
     @brand = brand
     # BRANDS << brand
-    BRANDS.include?("brand")
+    if BRANDS.include?(brand) false
       BRANDS << brand
-    
     end
+  end
 
   end
 
